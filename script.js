87188acd19467ed5c67e7a4cb162b645d29c3f6a
Doonywoony <javismcarr@gmail.com>
2021-12-10 00:21:42 -0500

    current conditions display complete

--- script.js
+++ script.js
@@ -40,7 +40,16 @@ function gatherWeather(cityName) {
 
         let lat = response.data.coord.lat;
         let lon = response.data.coord.long;
-        let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon" + lon + "&appid=" + APIKey + "&cnt="
+        let UVQueryURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon" + lon + "&appid=" + APIKey + "&cnt=1";
+        axios.get(UVQueryURl)
+        .then(function(response){
+            let uvindex = document.createElement("span");
+            uvindex.setAttribute("class", "badge badge-danger");
+            uvindex.innerHTML = response.data[0].value;
+            currentUVEl.innerHTML = "UV Index: ";
+            currentUVEl.append(uvindex);
+        })
+
      })
 
 }
